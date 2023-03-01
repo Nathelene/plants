6a331b01abd71ec05f7d847c052d207defd9f90b
Natalie Baker <nathelene76@gmail.com>
2023-03-01 14:33:06 -0700

    Add if statement

---
+++ plants.js
@@ -0,0 +1,8 @@
+var gets_sunshine = true;
+var ounces_of_water = 5;
+
+if (gets_sunshine && ounces_of_water >= 10) {
+    console.log("You're plant will thrive!")
+} else {
+    console.log("You're plant will die!")
+}
